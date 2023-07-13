fa79e5515ae5141c1eac7271ee22d45a6a2374be
somesh-dahatonde <sddahatonde22@gmail.com>
2023-07-14 00:07:59 +0530

    wsgl added

--- privateGPT.py
+++ privateGPT.py
@@ -91,6 +91,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
+from waitress import serve
 import os
 import time
 from constants import CHROMA_SETTINGS
@@ -138,74 +139,4 @@ def question_api():
     return jsonify({'answer': answer})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
-# import time
-# from flask import Flask, jsonify, request, Response
-# from dotenv import load_dotenv
-# from langchain.chains import RetrievalQA
-# from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-# from langchain.vectorstores import Chroma
-# from langchain.llms import GPT4All, LlamaCpp
-# import os
-
-# load_dotenv()
-
-# embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-# persist_directory = os.environ.get('PERSIST_DIRECTORY')
-# model_type = os.environ.get('MODEL_TYPE')
-# model_path = os.environ.get('MODEL_PATH')
-# model_n_ctx = os.environ.get('MODEL_N_CTX')
-# model_n_batch = int(os.environ.get('MODEL_N_BATCH', 8))
-# target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 4))
-# from constants import CHROMA_SETTINGS
-
-# app = Flask(__name__)
-
-# # Lazy initialization of the question-answering model
-# qa_instance = None
-
-# class RealTimeStreamingCallbackHandler(StreamingStdOutCallbackHandler):
-#     def send_output(self, output: str):
-#         response = Response(self.stream_output(output), mimetype='text/event-stream')
-#         response.headers.add('Cache-Control', 'no-cache')
-#         response.headers.add('Connection', 'keep-alive')
-#         response.headers.add('X-Accel-Buffering', 'no')
-#         return response
-
-#     def stream_output(self, output: str):
-#         for char in output:
-#             yield f"data: {char}\n\n"
-#             time.sleep(0.1)  # Adjust the delay between characters here
-
-# def get_question_answering_model():
-#     global qa_instance
-#     if qa_instance is None:
-#         embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-#         db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-#         retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-#         callbacks = [RealTimeStreamingCallbackHandler()]
-#         if model_type == "LlamaCpp":
-#             llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-#         elif model_type == "GPT4All":
-#             llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-#         else:
-#             raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
-
-#         qa_instance = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-
-#     return qa_instance
-
-# @app.route('/api/question', methods=['POST'])
-# def question_api():
-#     data = request.get_json()
-#     query = data.get('query')
-
-#     qa_model = get_question_answering_model()
-#     response = qa_model(query)
-#     answer = response['result']
-#     return jsonify({'answer': answer})
-
-# if __name__ == "__main__":
-#     app.run()
+    app.run()
